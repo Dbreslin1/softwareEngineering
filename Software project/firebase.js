5a28915cf0356740b16dbc1460a37bd23c35f883
Dbreslin1 <146346597+Dbreslin1@users.noreply.github.com>
2024-03-13 13:33:30 +0000

    Update firebase.js

--- Software project/firebase.js
+++ Software project/firebase.js
@@ -12,6 +12,7 @@ const logger = require("firebase-functions/logger");
 const admin = require("firebase-admin")
 const functions = require("firebase-functions")
 admin.initializeApp()
+
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
@@ -19,129 +20,259 @@ exports.helloWorld = onRequest((request, response) => {
     logger.info("Hello logs!", { structuredData: true });
     response.send("Hello from Firebase!");
 });
-//get cpus 
+
 exports.getCpu = functions.https.onRequest((request, response) => {
     // 1. Connect to our Firestore database
     let cpusToReturn = []
-    admin.firestore().collection('CPU').get().then((cpusSnapshot) => {
+    let query = admin.firestore().collection('CPU');
+
+    // Adding filters based on query parameters
+    if (request.query.Name) {
+        query = query.where('Name', '==', request.query.Name);
+    }
+    if (request.query.Cache) {
+        query = query.where('Cache', '==', request.query.Cache);
+    }
+    if (request.query.Cores) {
+        query = query.where('Cores', '==', parseInt(request.query.Cores));
+    }
+    if (request.query.IntegratedGraphics) {
+        query = query.where('IntegratedGraphics', '==', request.query.IntegratedGraphics);
+    }
+    if (request.query.Price) {
+        query = query.where('Price', '==', parseFloat(request.query.Price));
+    }
+    if (request.query.Speed) {
+        query = query.where('Speed', '==', parseFloat(request.query.Speed));
+    }
+    if (request.query.TPD) {
+        query = query.where('TPD', '==', parseInt(request.query.TPD));
+    }
+    if (request.query.Threads) {
+        query = query.where('Threads', '==', parseInt(request.query.Threads));
+    }
+
+    // Executing the query
+    query.get().then((cpusSnapshot) => {
         if (cpusSnapshot.empty) {
             console.log('No matching documents.');
             response.send('No data in database');
             return;
         }
         cpusSnapshot.forEach(doc => {
-            //get names of cpus from db using query
-            if (request.query.Name) {
-                if (request.query.Name == doc.data().Name) {
-                    cpusToReturn.push(doc.data());
-                }
-            } else {
-                cpusToReturn.push(doc.data());
-            }
-
+            cpusToReturn.push(doc.data());
         });
         // 2. Send data back to client
         response.send(cpusToReturn);
-    })
+    }).catch(error => {
+        console.error('Error getting documents:', error);
+        response.status(500).send('Error getting documents');
+    });
 });
+
 //get cases
 exports.getCase = functions.https.onRequest((request, response) => {
     // 1. Connect to our Firestore database
-    let caseToReturn = []
-    admin.firestore().collection('Case').get().then((caseSnapshot) => {
-        if (caseSnapshot.empty) {
+    let casesToReturn = []
+    let query = admin.firestore().collection('Case');
+
+    // Adding filters based on query parameters
+    if (request.query.Name) {
+        query = query.where('Name', '==', request.query.Name);
+    }
+    if (request.query['Fan Capacity']) {
+        query = query.where('FanCapacity', '==', parseInt(request.query['Fan Capacity']));
+    }
+    if (request.query['Fans Included']) {
+        query = query.where('FansIncluded', '==', request.query['Fans Included']);
+    }
+    if (request.query['Form Factor']) {
+        query = query.where('FormFactor', '==', request.query['Form Factor']);
+    }
+    if (request.query.Price) {
+        query = query.where('Price', '==', parseFloat(request.query.Price));
+    }
+    if (request.query.RGB) {
+        query = query.where('RGB', '==', request.query.RGB === 'true');
+    }
+    if (request.query['Storage Capacity']) {
+        query = query.where('StorageCapacity', '==', parseInt(request.query['Storage Capacity']));
+    }
+    if (request.query['Tempered Glass']) {
+        query = query.where('TemperedGlass', '==', request.query['Tempered Glass'] === 'true');
+    }
+
+    // Executing the query
+    query.get().then((casesSnapshot) => {
+        if (casesSnapshot.empty) {
             console.log('No matching documents.');
             response.send('No data in database');
             return;
         }
-        caseSnapshot.forEach(doc => {
-            //get names of case from db using query
-            if (request.query.Name) {
-                if (request.query.Name == doc.data().Name) {
-                    caseToReturn.push(doc.data());
-                }
-            } else {
-                caseToReturn.push(doc.data());
-            }
-
+        casesSnapshot.forEach(doc => {
+            casesToReturn.push(doc.data());
         });
         // 2. Send data back to client
-        response.send(caseToReturn);
-    })
+        response.send(casesToReturn);
+    }).catch(error => {
+        console.error('Error getting documents:', error);
+        response.status(500).send('Error getting documents');
+    });
 });
+
 //get gpus
 exports.getGpu = functions.https.onRequest((request, response) => {
     // 1. Connect to our Firestore database
-    let gpuToReturn = []
-    admin.firestore().collection('Gpu').get().then((gpuSnapshot) => {
-        if (gpuSnapshot.empty) {
+    let gpusToReturn = []
+    let query = admin.firestore().collection('Gpu');
+
+    // Adding filters based on query parameters
+    if (request.query.Name) {
+        query = query.where('Name', '==', request.query.Name);
+    }
+    if (request.query['Base Clock']) {
+        query = query.where('BaseClock', '==', parseFloat(request.query['Base Clock']));
+    }
+    if (request.query.DirectX) {
+        query = query.where('DirectX', '==', parseFloat(request.query.DirectX));
+    }
+    if (request.query.GDDR) {
+        query = query.where('GDDR', '==', parseFloat(request.query.GDDR));
+    }
+    if (request.query['Output Ports']) {
+        query = query.where('OutputPorts', '==', parseInt(request.query['Output Ports']));
+    }
+    if (request.query.PCIe) {
+        query = query.where('PCIe', '==', parseInt(request.query.PCIe));
+    }
+    if (request.query.Price) {
+        query = query.where('Price', '==', parseFloat(request.query.Price));
+    }
+    if (request.query.TDP) {
+        query = query.where('TDP', '==', parseInt(request.query.TDP));
+    }
+    if (request.query.VRAM) {
+        query = query.where('VRAM', '==', parseFloat(request.query.VRAM));
+    }
+
+    // Executing the query
+    query.get().then((gpusSnapshot) => {
+        if (gpusSnapshot.empty) {
             console.log('No matching documents.');
             response.send('No data in database');
             return;
         }
-        caseSnapshot.forEach(doc => {
-            //get names of gpus from db using query
-            if (request.query.Name) {
-                if (request.query.Name == doc.data().Name) {
-                    gpuToReturn.push(doc.data());
-                }
-            } else {
-                gpuToReturn.push(doc.data());
-            }
-
+        gpusSnapshot.forEach(doc => {
+            gpusToReturn.push(doc.data());
         });
         // 2. Send data back to client
-        response.send(gpuToReturn);
-    })
+        response.send(gpusToReturn);
+    }).catch(error => {
+        console.error('Error getting documents:', error);
+        response.status(500).send('Error getting documents');
+    });
 });
-//get motherboard
+
+//get motherboards
 exports.getMotherboard = functions.https.onRequest((request, response) => {
     // 1. Connect to our Firestore database
-    let motherboardToReturn = []
-    admin.firestore().collection('Motherboard').get().then((motherboardSnapshot) => {
-        if (motherboardSnapshot.empty) {
+    let motherboardsToReturn = []
+    let query = admin.firestore().collection('Motherboard');
+
+    // Adding filters based on query parameters
+    if (request.query.Name) {
+        query = query.where('Name', '==', request.query.Name);
+    }
+    if (request.query.Bluetooth) {
+        query = query.where('Bluetooth', '==', request.query.Bluetooth == 'true');
+    }
+    if (request.query.Chipset) {
+        query = query.where('Chipset', '==', request.query.Chipset);
+    }
+    if (request.query.Ethernet) {
+        query = query.where('Ethernet', '==', request.query.Ethernet === 'true');
+    }
+    if (request.query['Form Factor']) {
+        query = query.where('FormFactor', '==', request.query['Form Factor']);
+    }
+    if (request.query['M.2 Ports']) {
+        query = query.where('M2Ports', '==', parseInt(request.query['M.2 Ports']));
+    }
+    if (request.query['Output Ports']) {
+        query = query.where('OutputPorts', '==', parseInt(request.query['Output Ports']));
+    }
+    if (request.query['PCIe 3.0x1 Slots']) {
+        query = query.where('PCIe3x1Slots', '==', parseInt(request.query['PCIe 3.0x1 Slots']));
+    }
+    if (request.query['PCIe 3.0x16 Slots']) {
+        query = query.where('PCIe3x16Slots', '==', parseInt(request.query['PCIe 3.0x16 Slots']));
+    }
+    if (request.query['PCIe 4.0x16 Slots']) {
+        query = query.where('PCIe4x16Slots', '==', parseInt(request.query['PCIe 4.0x16 Slots']));
+    }
+    if (request.query.Price) {
+        query = query.where('Price', '==', parseFloat(request.query.Price));
+    }
+    if (request.query['RAM Slots']) {
+        query = query.where('RAMSlots', '==', parseInt(request.query['RAM Slots']));
+    }
+    if (request.query['Sata Ports']) {
+        query = query.where('SataPorts', '==', parseInt(request.query['Sata Ports']));
+    }
+    if (request.query['Socket Type']) {
+        query = query.where('SocketType', '==', request.query['Socket Type']);
+    }
+    if (request.query['USB 2.0 Ports']) {
+        query = query.where('USB2Ports', '==', parseInt(request.query['USB 2.0 Ports']));
+    }
+    if (request.query['USB 3.2 Ports']) {
+        query = query.where('USB32Ports', '==', parseInt(request.query['USB 3.2 Ports']));
+    }
+    if (request.query.WiFi) {
+        query = query.where('WiFi', '==', request.query.WiFi === 'true');
+    }
+
+    // Executing the query
+    query.get().then((motherboardsSnapshot) => {
+        if (motherboardsSnapshot.empty) {
             console.log('No matching documents.');
             response.send('No data in database');
             return;
         }
-        caseSnapshot.forEach(doc => {
-            //get names of motherboard from db using query
-            if (request.query.Name) {
-                if (request.query.Name == doc.data().Name) {
-                    motherboardToReturn.push(doc.data());
-                }
-            } else {
-                motherboardToReturn.push(doc.data());
-            }
-
+        motherboardsSnapshot.forEach(doc => {
+            motherboardsToReturn.push(doc.data());
         });
         // 2. Send data back to client
-        response.send(motherboardToReturn);
-    })
+        response.send(motherboardsToReturn);
+    }).catch(error => {
+        console.error('Error getting documents:', error);
+        response.status(500).send('Error getting documents');
+    });
 });
-//get psu
+
+//get psus
 exports.getPsu = functions.https.onRequest((request, response) => {
     // 1. Connect to our Firestore database
-    let psuToReturn = []
+    let psusToReturn = []
     admin.firestore().collection('Psu').get().then((psuSnapshot) => {
         if (psuSnapshot.empty) {
             console.log('No matching documents.');
             response.send('No data in database');
             return;
         }
-        caseSnapshot.forEach(doc => {
-            //get names of psu from db using query
+        psuSnapshot.forEach(doc => {
+            //get names of psus from db using query
             if (request.query.Name) {
                 if (request.query.Name == doc.data().Name) {
-                    psuToReturn.push(doc.data());
+                    psusToReturn.push(doc.data());
                 }
             } else {
-                psuToReturn.push(doc.data());
+                psusToReturn.push(doc.data());
             }
 
         });
         // 2. Send data back to client
-        response.send(psuToReturn);
+        response.send(psusToReturn);
     })
 });
 
@@ -155,8 +286,8 @@ exports.getRam = functions.https.onRequest((request, response) => {
             response.send('No data in database');
             return;
         }
-        caseSnapshot.forEach(doc => {
-            //get names of psu from db using query
+        ramSnapshot.forEach(doc => {
+            //get names of RAM from db using query
             if (request.query.Name) {
                 if (request.query.Name == doc.data().Name) {
                     ramToReturn.push(doc.data());
@@ -181,8 +312,8 @@ exports.getStorage = functions.https.onRequest((request, response) => {
             response.send('No data in database');
             return;
         }
-        caseSnapshot.forEach(doc => {
-            //get names of psu from db using query
+        storageSnapshot.forEach(doc => {
+            //get names of storage from db using query
             if (request.query.Name) {
                 if (request.query.Name == doc.data().Name) {
                     storageToReturn.push(doc.data());
