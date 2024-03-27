263edd83448dee1a2c8f4470985827124e87d2fb
AJCostello298 <146347417+AJCostello298@users.noreply.github.com>
2024-03-27 17:42:25 +0000

    Add files via upload
    Not fully working

---
+++ PreBuild.vue
@@ -0,0 +1,568 @@
+<template>
+    <body>
+      <div>
+        <header>
+          <div>
+            <nav>
+              <router-link to="/"><img src="@/assets/images/white-frame-free-png.png" class="logo" draggable="false"></router-link>
+              <ul>
+                <li><router-link to="/Builder" draggable="false">Builder</router-link></li>
+                <li><router-link to="/Comparison" draggable="false">Comparison</router-link></li>
+                <li><router-link to="/PreBuild"><a>Build by Budget</a></router-link></li>
+                <li><router-link to="/Support" draggable="false">Support</router-link></li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+        <h1>Choose Your Budget</h1>
+      </div>
+      <div class="centered">
+      <select v-model="selectedBudget">
+        <option value="750">€750</option>
+        <option value="1000">€1000</option>
+        <option value="1250">€1250</option>
+      </select>
+    </div>
+      <button class="btn" @click="build">BUILD</button>
+      <div>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th style="width: 20%">Component</th>
+              <th>Part Name</th>
+              <th style="width: 100px;">Price</th>
+              <th style="width: 100px;">Compatibility</th>
+              <th style="width: 100px;">Link</th> 
+            </tr>
+          </thead>
+          <tbody>
+            <tr> 
+              <td><router-link to="/Comparison">Motherboard</router-link></td>
+              <td>
+                {{ selectedMotherboard ? selectedMotherboard.Name: ''}}
+              </td>
+              <td>{{ selectedMotherboard ? selectedMotherboard.Price: '' }}</td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td><router-link to="/Comparison">CPU</router-link></td>
+              <td>
+                <!-- {{ selectedCpu ? selectedCpu.Name: '' }}-->
+              </td>
+              <td>{{ selectedCpu ? selectedCpu.Price: '' }}</td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr> 
+              <td><router-link to="/Comparison">Case</router-link></td>
+              <td>
+                <!--{{ selectedCase.Name }}-->
+              </td>
+              <td>{{ selectedCase ? selectedCase.Price : '' }}</td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr> 
+              <td><router-link to="/Comparison">GPU</router-link></td>
+              <td>
+                <!--{{ selectedGpu.Name }}-->
+              </td>
+                <td>{{ selectedGpu ? selectedGpu.Price : '' }}</td> <!-- Make sure selectedGpuPrice is correctly bound here -->
+                <td></td>
+                <td></td>
+            </tr>
+            <tr> 
+              <td><router-link to="/Comparison">RAM</router-link></td>
+              <td>
+                 <!--{{ selectedRAM.Name }}-->
+              </td>
+              <td>{{ selectedRAM ? selectedRAM.Price : '' }}</td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr> 
+              <td><router-link to="/Comparison">Storage</router-link></td>
+              <td>
+                 <!--{{ selectedStorage.Name }}-->
+              </td>
+              <td>{{ selectedStorage ? selectedStorage.Price : '' }}</td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr> 
+              <td><router-link to="/Comparison">PSU</router-link></td>
+              <td>
+                 <!--{{ selectedPSU.Name }}-->
+              </td>
+              <td>{{ selectedPSU ? selectedPSU.Price : '' }}</td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr class="total-row">
+    <td>Total</td>
+    <td></td>
+    <td>{{ totalPrice }}</td>
+    <td>
+      <div v-if="compatibility !== null" :class="{'status': true, 'compatible': compatibility === 'Compatible', 'incompatible': compatibility !== 'Compatible'}">
+        <span v-if="compatibility === 'Compatible'">✔</span>
+        <span v-else>✘</span>
+      </div>
+    </td>
+  </tr>
+          </tbody>
+        </table>
+    </body>
+  </template>
+  
+  <script>
+export default {
+  name: 'ChooseYourParts',
+  data() {
+    return {
+      cpus: [],
+      motherboards: [],
+      cases: [],
+      gpus: [],
+      rams: [],
+      storages: [],
+      psus: [],
+      selectedGpu: null,  // Initialize selectedGpu to null
+      selectedCase: null,  // Initialize selectedGpu to null
+      selectedCpu: null,
+      selectedMotherboard: null,
+      selectedRAM: null,
+      selectedStorage: null,
+      selectedPSU: null,
+      compatibility: null,
+      totalPrice: 0
+      
+    };
+  },
+  mounted() {
+    this.fetchCPUs();
+    this.fetchMotherboards();
+    this.fetchCases();
+    this.fetchGpus();
+    this.fetchRAM();
+    this.fetchStorages();
+    this.fetchPsus();
+  },
+  methods: {
+    async fetchCPUs() {
+      try {
+        const response = await fetch('https://getcpu-igki44h7vq-uc.a.run.app');
+        const data = await response.json();
+        console.log(data); // Log the response data to the console
+        this.cpus = data; // Assuming data is an array of CPU objects with 'id' and 'Name' properties
+      } catch (error) {
+        console.error('Error fetching CPUs:', error);
+      }
+    },
+    async fetchMotherboards() {
+      try {
+        const response = await fetch('https://getmotherboard-igki44h7vq-uc.a.run.app');
+        const data = await response.json();
+        console.log(data); // Log the response data to the console
+        this.motherboards = data; // Assuming data is an array of motherboard objects with 'id' and 'Name' properties
+      } catch (error) {
+        console.error('Error fetching motherboards:', error);
+      }
+    },
+    async fetchCases() {
+      try {
+        const response = await fetch('https://getcase-igki44h7vq-uc.a.run.app');
+        const data = await response.json();
+        console.log(data); // Log the response data to the console
+        this.cases = data; // Assuming data is an array of cases objects with 'id' and 'Name' properties
+      } catch (error) {
+        console.error('Error fetching Cases:', error);
+      }
+    },
+    async fetchGpus() {
+      try {
+        const response = await fetch('https://getgpu-igki44h7vq-uc.a.run.app');
+        const data = await response.json();
+        console.log(data); // Log the response data to the console
+        this.gpus = data; // Assuming data is an array of Gpus objects with 'id' and 'Name' properties
+      } catch (error) {
+        console.error('Error fetching Gpus:', error);
+      }
+    },
+    
+    
+    async fetchRAM() {
+      try {
+        const response = await fetch('https://getram-igki44h7vq-uc.a.run.app');
+        const data = await response.json();
+        console.log(data); // Log the response data to the console
+        this.rams = data; // Assuming data is an array of rams objects with 'id' and 'Name' properties
+      } catch (error) {
+        console.error('Error fetching Rams:', error);
+      }
+    },
+    async fetchStorages() {
+      try {
+        const response = await fetch('https://getstorage-igki44h7vq-uc.a.run.app');
+        const data = await response.json();
+        console.log(data); // Log the response data to the console
+        this.storages = data; // Assuming data is an array of storages objects with 'id' and 'Name' properties
+      } catch (error) {
+        console.error('Error fetching Storages:', error);
+      }
+    },
+    async fetchPsus() {
+      try {
+        const response = await fetch('https://getpsu-igki44h7vq-uc.a.run.app');
+        const data = await response.json();
+        console.log(data); // Log the response data to the console
+        this.psus = data; // Assuming data is an array of Psus objects with 'id' and 'Name' properties
+      } catch (error) {
+        console.error('Error fetching Psus:', error);
+      }
+    },
+    
+
+    checkComp() {
+      if (this.selectedCpu && this.selectedMotherboard) {
+        if ((this.selectedCpu.Compatibility === this.selectedMotherboard.Compatibility) || (this.selectedCpu.Compatibility === this.selectedMotherboard.Compatibility1)) {
+          this.compatibility = 'Compatible';
+        } else {
+          this.compatibility = 'Not Compatible';
+        }
+      } else {
+        this.compatibility = null; // Reset to null if button is not pressed
+      }
+
+      this.totalPrice = this.calculateTotalPrice();
+      
+    },
+    calculateTotalPrice() {
+      let totalPrice = 0;
+     if (this.selectedMotherboard) {
+    totalPrice += parseFloat(this.selectedMotherboard.Price);
+  }
+  if (this.selectedCpu) {
+    totalPrice += parseFloat(this.selectedCpu.Price);
+  }
+  if (this.selectedGpu) {
+    totalPrice += parseFloat(this.selectedGpu.Price);
+  }
+  
+  if (this.selectedCase) {
+    totalPrice += parseFloat(this.selectedCase.Price);
+  }
+  if (this.selectedRAM) {
+    totalPrice += parseFloat(this.selectedRAM.Price);
+  }
+  if (this.selectedPSU) {
+    totalPrice += parseFloat(this.selectedPSU.Price);
+  }
+  if (this.selectedStorage) {
+    totalPrice += parseFloat(this.selectedStorage.Price);
+  }
+ return totalPrice;
+}
+  }
+}
+
+</script>
+  
+  <style scoped>
+  body {
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      font-size: 18px;
+      margin: 0;
+      padding: 0;
+      background-color: #3f3f3f;
+      height: 100vh;
+      overflow-x: hidden;
+  }
+  .btn {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 5px;
+    margin-top: 5%;
+    margin-left: 47.5%;
+  }
+  
+  nav {
+      display: flex;
+      align-items: center;
+      padding: 20px 10%;
+      background-color: #2f2e2d;
+  }
+  
+  #homenav {
+      padding-bottom: 100px;
+  }
+  
+  nav .menu {
+      width: 25px;
+      margin-right: 20px;
+      cursor: pointer;
+  }
+  
+  nav .logo {
+      width: 60px;
+      cursor: pointer;
+      z-index: 5;
+  }
+  
+  nav ul {
+      flex: 1;
+      text-align: right;
+  }
+  
+  nav ul li {
+      display: inline-block;
+      list-style: none;
+      margin: 0 20px;
+  }
+  
+  nav ul li a {
+      text-decoration: none;
+      color: white;
+  
+  }
+  
+  nav ul li a:hover {
+      color: #fadf7f;
+  }
+  
+  h1 {
+      color: #ffffff;
+      background-color: #294d4a;
+      margin-top: 0px;
+      padding-top: 1rem;
+      padding-bottom: 1rem;
+      font-size: 140%;
+      text-align: center;
+  }
+  
+  .text-container h2 {
+      color: #ffffff;
+      font-size: 700%;
+      font-family: 'Anton', sans-serif;
+      line-height: 100%;
+      margin-left: 10%;
+      margin-top: 0px;
+      margin-bottom: 30px;
+  }
+  
+  .text-container p {
+      margin-top: 0px;
+      margin-left: 10%;
+      color: #9da2ab;
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      font-size: 20px;
+  }
+  
+  .text-container a {
+      text-decoration: none;
+      background: #5fbb97;
+      padding: 14px 40px;
+      margin-left: 10%;
+      display: inline-block;
+      color: #fff;
+      font-size: 20px;
+      border-radius: 30px;
+  }
+  
+  section {
+      margin: 0;
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      min-height: 0px;
+      padding: 0px 10%;
+  }
+  
+  .curve {
+      position: absolute;
+      height: 225px;
+      width: 100%;
+      bottom: 0;
+  
+  }
+  
+  .curve::before {
+      content: '';
+      display: block;
+      position: absolute;
+      border-radius: 100% 50%;
+      width: 54.3%;
+      height: 100%;
+      background-color: #3f3f3f;
+      transform: translate(85%, 137%);
+  }
+  
+  .curve::after {
+      content: '';
+      display: block;
+      position: absolute;
+      border-radius: 100% 50%;
+      width: 51.7%;
+      height: 100%;
+      background-color: #2f2e2d;
+      transform: translate(-4%, 130%);
+      z-index: -1;
+  }
+  
+  table {
+      margin-top: 50px;
+      border-collapse: collapse;
+      width: 70%;
+      margin-left: 15%;
+      box-shadow: 0 .1rem 1rem #000;
+      border-radius: 1rem;
+      color: #fff;
+      /*border: 3px double hotpink;*/
+  }
+  
+  th,
+  td {
+      height: 60px;
+      padding: 0.6rem;
+      text-align: left;
+      /*border: 1px dashed hotpink;*/
+  }
+  
+  th {
+      background-color: #2f2e2d;
+  }
+  
+  tbody tr:hover {
+      background-color: #3336;
+  }
+  
+  .status {
+      margin-left: 37%;
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      text-align: center;
+      box-shadow: 0 .1rem .2rem #000;
+  }
+  
+  .status.compatiable {
+      background-color: #2a6863;
+  }
+  
+  .status.uncompatiable {
+      background-color: #ca3c3c;
+  }
+  
+  a {
+      color: #fff;
+      text-decoration: none;
+  }
+  
+  a:hover {
+      color: #fadf7f;
+  }
+  
+  .image {
+      height: 30vmin;
+      width: 40vmin;
+      object-fit: cover;
+      object-position: center;
+      border-radius: 15%;
+      filter: drop-shadow(10px 10px 10px #2d2f2e)
+  }
+  
+  #image-setone {
+      display: flex;
+      gap: 10vmin;
+      position: absolute;
+      left: 49%;
+      top: 15%;
+      border-radius: 100% 50%;
+  }
+  
+  #image-settwo {
+      display: flex;
+      gap: 10vmin;
+      position: absolute;
+      left: 38%;
+      top: 35%;
+      border-radius: 100% 50%;
+  }
+  
+  #image-setthree {
+      display: flex;
+      gap: 10vmin;
+      position: absolute;
+      left: 49%;
+      top: 60%;
+      border-radius: 100% 50%;
+  }
+  
+  select {
+      box-shadow: 0 .1rem .3rem #000;
+      width: 500px;
+      font-size: 18px;
+      padding: 0.3em 1.5em;
+      background: #2f2e2d;
+      color: #fff;
+      border: none;
+  }
+  
+  .custom-select {
+      margin-top: 20px;
+      margin-left: 45%;
+      margin-right: 45%;
+  }
+  
+  .rectangle-container {
+      display: flex;
+      justify-content: space-between;
+  }
+  
+  .rectangle {
+      flex: 1;
+      height: 700px;
+      background-color: #2f2e2d;
+      margin: 5%;
+      border-radius: 1% 1%;
+      filter: drop-shadow(10px 10px 10px #2f2e2d)
+  }
+  
+  .info {
+      position: absolute;
+      top: 4%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      color: #ffffff
+  }
+  .status {
+    font-size: 20px;
+  }
+  
+  .compatible {
+    color: green;
+  }
+  
+  .incompatible {
+    color: red;
+  }
+  
+  tr.total-row {
+    border-top: 1px solid white;
+  }
+  
+.centered {
+  text-align: center;
+  align-items: center; /* Set the container height to full viewport height */
+}
+  </style>
