3b1d7195b39f6704f1e73b6b82ee0fdd46d19f5c
iangill1 <146347592+iangill1@users.noreply.github.com>
2024-03-27 12:11:31 +0000

    Update routes.js

--- router/routes.js
+++ router/routes.js
@@ -6,5 +6,10 @@ export default [
   { path: '/', component: loadPage('Home') }, // Add a comma here
   { path: '/Builder', component: loadPage('Builder') },
   { path: '/Comparison', component: loadPage('Comparison') },
-  { path: '/Support', component: loadPage('Support') }
+  { path: '/Support', component: loadPage('Support') },
+  { path: '/Register', component: loadPage('Register')},
+  { path: '/Login', component: loadPage('Login')},
+  { path: '/HomePage', component: loadPage('HomePage')}
+
+
 ]
